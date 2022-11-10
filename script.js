c106b587fa41dfd11148f10fdd34b259b51f32e7
edwardcodelab <56959323+edwardcodelab@users.noreply.github.com>
2022-11-11 01:54:17 +0800

    Update script.js

--- script.js
+++ script.js
@@ -43,7 +43,7 @@ function intextsearch(){
                     height: '100%'
                })
                .html(
-                    '<select id="itsearchengine"><option value="https://www.freecollocation.com/search?word=">freecollocation</option><option value="https://duckduckgo.com/?q=">duckduckgo</option><option value="https://en.wikipedia.org/wiki/">Wikipedia</option><br></select><iframe id="intextsearchurl" height="500px" width="100%" class="page group" overflow="wrap">'
+                    '<select id="itsearchengine"><option value="https://www.wolframalpha.com/input?i=">wolframalpha</option><option value="https://www.bing.com/search?q=">Bing</option><option value="https://swisscows.com/en/web?query=">SwissCows</option><option value="https://en.wikipedia.org/wiki/">Wikipedia</option><br></select><option value="https://www.freecollocation.com/search?word=">freecollocation</option><iframe id="intextsearchurl" height="500px" width="100%" class="page group" overflow="wrap">'
                     )
                .parent()
                .attr('id','intextsearchid')
